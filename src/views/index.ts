cff6d204991e0e4ce41ee7b06b98f5875a93b4fc
HonLuk <luhan09@qq.com>
2022-03-29 21:59:05 +0800

    fixed

--- src/views/index.ts
+++ src/views/index.ts
@@ -10,98 +10,116 @@ monaco.languages.register({ id: "typescript" });
 import { loadWASM } from "onigasm";
 import json2ts from "./json2ts";
 import ClipboardJS from "clipboard";
-
+import { editor } from "monaco-editor";
+import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
+let inputInstance: IStandaloneCodeEditor, outputInstance: IStandaloneCodeEditor;
 const themeMap = {
   dark: "vs-dark",
   light: "vs"
 };
 const init = async (theme: keyof typeof themeMap) => {
-  //校验
-  await loadWASM(`${base}/asset/onigasm/onigasm.wasm`);
-  //不做提示
-  (window as any).MonacoEnvironment = {
-    getWorkerUrl: function(moduleId, label) {
-      // console.log(moduleId, label);
-      // if (label === "json") {
-      //   return `${base}/asset/monaco/json.worker.bundle.js`;
-      // }
-      // if (label === "typescript" || label === "javascript") {
-      //   return `${base}/asset/monaco/ts.worker.bundle.js`;
-      // }
-      return `${base}/asset/monaco/editor.worker.bundle.js`;
-    }
-  };
-  //语法
-  const inputRegistry = new Registry({
-    getGrammarDefinition: async scopeName => {
-      return {
-        format: "json", // 语法文件格式，有json、plist
-        content: await (await fetch(`${base}/asset/grammars/JSON.tmLanguage.json`)).text()
-      };
-    }
-  });
-  const outputRegistry = new Registry({
-    getGrammarDefinition: async scopeName => {
-      return {
-        format: "json", // 语法文件格式，有json、plist
-        content: await (await fetch(`${base}/asset/grammars/TypeScript.tmLanguage.json`)).text()
-      };
-    }
-  });
-  const inputGrammars = new Map();
-  inputGrammars.set("json", "source.json");
-  const outputGrammars = new Map();
-  outputGrammars.set("typescript", "source.ts");
+  if (!inputInstance && !outputInstance) {
+    //校验
+    await loadWASM(`${base}/asset/onigasm/onigasm.wasm`);
+    //不做提示
+    (window as any).MonacoEnvironment = {
+      getWorkerUrl: function(moduleId, label) {
+        // console.log(moduleId, label);
+        // if (label === "json") {
+        //   return `${base}/asset/monaco/json.worker.bundle.js`;
+        // }
+        // if (label === "typescript" || label === "javascript") {
+        //   return `${base}/asset/monaco/ts.worker.bundle.js`;
+        // }
+        return `${base}/asset/monaco/editor.worker.bundle.js`;
+      }
+    };
+    //语法
+    const inputRegistry = new Registry({
+      getGrammarDefinition: async scopeName => {
+        return {
+          format: "json", // 语法文件格式，有json、plist
+          content: await (await fetch(`${base}/asset/grammars/JSON.tmLanguage.json`)).text()
+        };
+      }
+    });
+    const outputRegistry = new Registry({
+      getGrammarDefinition: async scopeName => {
+        return {
+          format: "json", // 语法文件格式，有json、plist
+          content: await (await fetch(`${base}/asset/grammars/TypeScript.tmLanguage.json`)).text()
+        };
+      }
+    });
+    const inputGrammars = new Map();
+    inputGrammars.set("json", "source.json");
+    const outputGrammars = new Map();
+    outputGrammars.set("typescript", "source.ts");
 
-  //编辑器初始化
-  const inputInstance = monaco.editor.create(document.querySelector(".input-editor")!, {
-    value: "",
-    language: "json",
-    theme: themeMap[theme],
-    fontSize: 14,
-    minimap: {
-      enabled: false
-    },
-    lineNumbersMinChars: 2
-  });
-  const outputInstance = monaco.editor.create(document.querySelector(".output-editor")!, {
-    value: "",
-    language: "typescript",
-    theme: themeMap[theme],
-    fontSize: 14,
-    minimap: {
-      enabled: false
-    },
-    lineNumbersMinChars: 2
-  });
+    //编辑器初始化
+    inputInstance = monaco.editor.create(document.querySelector(".input-editor")!, {
+      value: "",
+      language: "json",
+      theme: themeMap[theme],
+      fontSize: 14,
+      minimap: {
+        enabled: false
+      },
+      lineNumbersMinChars: 2
+    });
+    outputInstance = monaco.editor.create(document.querySelector(".output-editor")!, {
+      value: "",
+      language: "typescript",
+      theme: themeMap[theme],
+      fontSize: 14,
+      minimap: {
+        enabled: false
+      },
+      lineNumbersMinChars: 2
+    });
 
-  if (theme === "dark") {
-    //主题
-    const OneDark = await (await fetch(`${base}/asset/theme/theme.json`)).json();
-    // 定义主题
-    monaco.editor.defineTheme("OneDark", OneDark);
-    // 使用定义的主题
-    monaco.editor.setTheme("OneDark");
+    if (theme === "dark") {
+      //主题
+      const OneDark = await (await fetch(`${base}/asset/theme/theme.json`)).json();
+      // 定义主题
+      monaco.editor.defineTheme("OneDark", OneDark);
+      // 使用定义的主题
+      monaco.editor.setTheme("OneDark");
+    }
+    inputInstance.onDidChangeModelContent(() => {
+      outputInstance.setValue(json2ts(inputInstance.getValue()));
+      setCopyButton(theme, outputInstance);
+    });
+    await Promise.all([
+      wireTmGrammars(monaco, inputRegistry, inputGrammars, inputInstance),
+      wireTmGrammars(monaco, outputRegistry, outputGrammars, outputInstance)
+    ]);
+    window.onresize = function() {
+      inputInstance.layout();
+      outputInstance.layout();
+    };
   }
-  inputInstance.onDidChangeModelContent(() => {
-    outputInstance.setValue(json2ts(inputInstance.getValue()));
-    setCopyButton(theme, outputInstance);
-  });
-  await Promise.all([
-    wireTmGrammars(monaco, inputRegistry, inputGrammars, inputInstance),
-    wireTmGrammars(monaco, outputRegistry, outputGrammars, outputInstance)
-  ]);
-  window.onresize = function() {
-    inputInstance.layout();
-    outputInstance.layout();
-  };
   //处理刚进入
-  let clipboardData = "";
-
-  try {
-    clipboardData = await navigator.clipboard.readText();
-    inputInstance.setValue(clipboardData);
-  } catch (e) {}
+  inputInstance.setValue("");
+  inputInstance.setScrollTop(0);
+  if (window.utools) {
+    setTimeout(() => {
+      inputInstance.focus();
+      if (window.utools.isMacOs()) {
+        // macos 模拟粘贴
+        utools.simulateKeyboardTap("v", "command");
+      } else {
+        // windows linux 模拟粘贴
+        utools.simulateKeyboardTap("v", "ctrl");
+      }
+    });
+  } else {
+    let clipboardData = "";
+    try {
+      clipboardData = await navigator.clipboard.readText();
+      inputInstance.setValue(clipboardData);
+    } catch (e) {}
+  }
 };
 
 //设置复制按钮
@@ -121,7 +139,10 @@ function setCopyButton(theme: keyof typeof themeMap, outputInstance: any) {
   }
 
   copyButton.onclick = () => {
-    utools.copyText(outputInstance.getValue());
+    if (window.utools) {
+      window.utools.copyText(outputInstance.getValue());
+      window.utools.showNotification("复制成功");
+    }
     new ClipboardJS(copyButton, {
       text: function(trigger) {
         return outputInstance.getValue();
@@ -132,8 +153,8 @@ function setCopyButton(theme: keyof typeof themeMap, outputInstance: any) {
 }
 
 if (window.utools) {
-  utools.onPluginEnter(() => {
-    init(utools.isDarkColors() ? "dark" : "light");
+  window.utools.onPluginEnter(() => {
+    init(window.utools.isDarkColors() ? "dark" : "light");
   });
 } else {
   init("dark");
